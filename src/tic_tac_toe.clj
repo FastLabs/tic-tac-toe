08313f15571b6ef444bbabe1d159047749cf9121
Oleg Bulavitchi <bulavitchi@gmail.com>
2024-08-02 15:30:56 +0100

    game board validation starts

---
+++ src/tic_tac_toe.clj
@@ -0,0 +1,32 @@
+(ns tic-tac-toe)
+
+(def win-sets
+  [[0 1 2]
+   [3 4 5]
+   [6 7 8]
+   [0 3 6]
+   [1 4 7]
+   [2 5 8]
+   [0 4 8]
+   [2 4 6]])
+
+(defn- winning-line? [board winning-positions]
+  (and (apply = (map #(nth board %) winning-positions))
+       (not (nil? (first winning-positions)))))
+
+(defn- find-sequence-winner [board winning-positions]
+  (when (winning-line? board winning-positions)
+    (board (first winning-positions))))
+
+(defn new-game-board []
+  (vec (repeat 9 nil)))
+
+
+(defn board-full? [board]
+  (not-any? nil? board))
+
+(defn find-game-winner [board]
+  (let [game-winner (some #(find-sequence-winner board %) win-sets)]
+    (or game-winner
+        (board-full? board))))
+
---
+++ test/tic_tac_toe_test.clj
@@ -0,0 +1,62 @@
+(ns tic-tac-toe-test
+  (:require [clojure.test :refer [deftest testing is are]]
+            [tic-tac-toe :as game]))
+
+(deftest tic-tac-toe-game-test
+  (testing "the finite set of winning combinations"
+    (is (= #{[0 1 2]
+             [3 4 5]
+             [6 7 8]
+             [0 3 6]
+             [1 4 7]
+             [2 5 8]
+             [0 4 8]
+             [2 4 6]}
+           (set game/win-sets))))
+
+  (testing "board allocation"
+    (is (= [nil nil nil
+            nil nil nil
+            nil nil nil]
+           (game/new-game-board))))
+
+  (testing "board-full? returns true when the game board is filled by the players"
+    (let [game-board [:x :o :x :o :x :x :o :x :x]]
+      (is (game/board-full? game-board))))
+
+  (testing "board-full? returns false for an incomplete or  empty board"
+    (testing "an empty new board"
+      (is (not (game/board-full? (game/new-game-board)))))
+
+    (testing "a partially filled game board"
+      (let [game-board [:x :o :x :o nil :x :o :x nil]]
+        (is (not (game/board-full? game-board))))))
+
+  (testing "winning combinations"
+    (are [board] (= :x (game/find-game-winner board))
+                 [:x :x :x
+                  nil :o :o
+                  nil nil nil]
+                 [:o :o nil
+                  :x :x :x
+                  nil nil nil]
+                 [nil nil nil
+                  :o nil :o
+                  :x :x :x]
+                 [:x nil :o
+                  :x :o nil
+                  :x nil nil]
+                 [:o :x :o
+                  nil :x nil
+                  nil :x nil]
+                 [:o nil :x
+                  nil :o :x
+                  nil nil :x]
+                 [:x nil :o
+                  nil :x nil
+                  :o nil :x]
+                 [:o nil :x
+                  nil :x :o
+                  :x nil nil])))
+
+
